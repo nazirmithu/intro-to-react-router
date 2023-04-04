94f5a5560d40d15539d524e6df315b41b2f3fb34
nazirmithu <nazirmithu22@gmail.com>
2023-04-04 18:51:45 +0600

    first commit

---
+++ src/components/Friend/Friend.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Friend.css'
+import { Link } from 'react-router-dom';
+const Friend = ({friend}) => {
+    // console.log(friend)
+    const {name, id, email, phone} = friend;
+    return (
+        <div className='friend'>
+            <h3>{name}</h3>
+            <p>Email: {email}</p>
+            <p>Phone: {phone}</p>
+            <p><Link to={`/friend/${id}`}>show me details</Link></p>
+        </div>
+    );
+};
+
+export default Friend;
